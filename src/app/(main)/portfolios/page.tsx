7076f4df7727b4fe6f7a6f8486b79838a9e31020
gurashishgill171 <ggill_be19@thapar.edu>
2025-03-29 10:39:33 +0530

    front page ui

---
+++ src/app/(main)/portfolios/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <main>Here will be all the portfolios</main>;
+}
+
+export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,29 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const roboto = Roboto({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: {
-		template: "%s - AI Portfolio Builder",
-		absolute: "AI Portfolio Builder",
-	},
-	description:
-		"An AI-powered portfolio builder that helps professionals and creatives instantly generate stunning, personalized portfolios. Showcase your work, experience, and skills with AI-driven designs and content suggestions. No coding or design skills required—just describe your vision, and let AI do the rest!",
+  title: {
+    template: "%s - AI Portfolio Builder",
+    absolute: "AI Portfolio Builder",
+  },
+  description:
+    "An AI-powered portfolio builder that helps professionals and creatives instantly generate stunning, personalized portfolios. Showcase your work, experience, and skills with AI-driven designs and content suggestions. No coding or design skills required—just describe your vision, and let AI do the rest!",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={roboto.className}>{children}</body>
-		</html>
-	);
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={roboto.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
