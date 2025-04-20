8a3dbc3464977f4521c17151549c91f5885442ab
gurashishgill171 <ggill_be19@thapar.edu>
2025-04-20 20:04:11 +0530

    some changes

--- src/app/(main)/editor/actions.ts
+++ src/app/(main)/editor/actions.ts
@@ -6,20 +6,39 @@ import { auth } from "@clerk/nextjs/server";
 
 export async function savePortfolio({ portfolioData }: PortfolioEditorProps) {
   // either create or update portfolio
-  const {id} = portfolioData;
-  const {userId} = await auth();
+  const { id } = portfolioData;
+  const { userId } = await auth();
 
   if (!userId) {
     throw new Error("User not authenticated");
   }
 
-  if(!id) {
+  if (!id) {
     return prisma.portfolio.create({
       data: {
         ...portfolioData,
+        photo: "",
         userId,
-        
-      }
-    })
+        workExperiences: {
+          create: portfolioData.workExperiences?.map((workExperience) => ({
+            ...workExperience,
+            startDate: new Date(workExperience.startDate),
+            endDate: new Date(workExperience.endDate ?? ""),
+          })),
+        },
+        educations: {
+          create: portfolioData.educations?.map((education) => ({
+            ...education,
+            startDate: new Date(education.startDate),
+            endDate: new Date(education.endDate ?? ""),
+          })),
+        },
+        projects: {
+          create: portfolioData.projects?.map((project) => ({
+            ...project,
+          })),
+        }
+      },
+    });
   }
 }
